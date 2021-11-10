d54843f6d1dc498957619568408499a0d04964e3
Tulikasharma <tulikasharma368@gmail.com>
2021-11-10 17:30:29 +0530

    [Tulika]practice: redux in single file

--- src/index.js
+++ src/index.js
@@ -3,6 +3,49 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createStore} from "redux";
+
+//action
+const increment = ()=>{
+  return {
+    type: 'INCREMENT'
+  }
+}
+
+const decrement = () =>{
+  return{
+    type: 'DECREMENT'
+  }
+}
+
+//reducer
+const counter = (state=0, action)=>{
+  switch(action.type){
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+  }
+}
+
+
+//store
+let store = createStore(counter);
+
+//display store
+store.subscribe(()=>console.log(store.getState()));
+
+//dispatch
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(decrement());
+store.dispatch(decrement());
+store.dispatch(increment());
+
+
+
+
 
 ReactDOM.render(
   <React.StrictMode>
