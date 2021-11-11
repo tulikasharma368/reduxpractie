8ff645ab2256e5f25911f7c86e7f1d80a8d599d7
Tulikasharma <tulikasharma368@gmail.com>
2021-11-11 22:57:02 +0530

    [Tulika]implemented: redux using multiple files and used payload

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ function App() {
   return (
     <div >
       <h1>Counter {counter}</h1>
-      <button onClick={()=>dispatch(increment())}>Increase</button>
+      {/* increment will happen by 8 bcz of the payload */}
+      <button onClick={()=>dispatch(increment(8))}>Increase</button>
       <button onClick={()=>dispatch(decrement())}>Decrease</button>
       {isLogged?<h3>Login Information</h3>:<h3>Not logged in</h3>}
       
